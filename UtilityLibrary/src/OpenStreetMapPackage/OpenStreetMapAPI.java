c724aee44cb05eccc47ae5e6e6775ce8d6aa60fd
DanieleRonco <82151369+DanieleRonco@users.noreply.github.com>
2022-03-18 21:05:05 +0100

    Libreria "Utility"
    Creazione della libreria "Utility" che riunisce in un'unica libreria tutte quelle precedentemente create.

---
+++ UtilityLibrary/src/OpenStreetMapPackage/OpenStreetMapAPI.java
@@ -0,0 +1,108 @@
+package OpenStreetMapPackage;
+
+import HTTPPackage.HHttp;
+import XMLParserPackage.XMLParser;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Daniele Roncoroni
+ */
+public class OpenStreetMapAPI {
+    //ATTRIBUTI
+    private String xmlFile;
+    private Document document;
+    private XMLParser parser;
+    
+    //COSTRUTTORI
+    //Costruttore di default
+    public OpenStreetMapAPI(){
+        this.xmlFile = "";
+        this.document = null;
+        this.parser = new XMLParser();
+    }
+    //Costruttore parametrico - come solo parametro xmlFile
+    public OpenStreetMapAPI(String XmlFile){
+        this.xmlFile = XmlFile;
+        this.document = null;
+        this.parser = new XMLParser(xmlFile);
+    }
+
+    //GET
+    public String getXmlFile() {
+        return xmlFile;
+    }
+    public Document getDocument(){
+        return this.document;
+    }
+    public XMLParser getParser(){
+        return this.parser;
+    }
+
+    //SET
+    public void setXmlFile(String xmlFile) {
+        this.xmlFile = xmlFile;
+    }
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+    public void setParser(XMLParser parser){
+        this.parser = parser;
+    }
+    
+    //METODI
+    public OCoordinate TrovaCoordinate(String luogo) {
+        HHttp h = new HHttp();
+        String stringaURL = null;
+        try {
+            stringaURL = "https://nominatim.openstreetmap.org/search?q=" + h.encodeValue(luogo) + "&format=xml&addressdetails=1";
+
+            parser.XMLFileBuilding(h.Richiesta(stringaURL));
+        
+            NodeList lista = parser.getElements("place");
+            if(lista != null){
+                Element elemento = (Element)lista.item(0);
+                return new OCoordinate(elemento.getAttribute("lat"), elemento.getAttribute("lon"));
+            } else return null;        
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(OpenStreetMapAPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;  
+    }
+    
+    public double DistanzaTraDuePunti(OCoordinate c1, OCoordinate c2){
+        double R = 6371; //raggio terrestre (km)
+        double dLat = deg2rad(c2.getLatitudine()-c1.getLatitudine());
+        double dLon = deg2rad(c2.getLongitudine()-c1.getLongitudine());
+        double a = 
+          Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(deg2rad(c1.getLatitudine())) * Math.cos(deg2rad(c2.getLatitudine())) * 
+          Math.sin(dLon/2) * Math.sin(dLon/2)
+          ; 
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        return R * c; //distanza (km)
+    }
+
+    private double deg2rad(double deg) {
+        return deg * (Math.PI/180);
+    }
+}
---
+++ UtilityLibrary/src/TelegramPackage/TUpdate.java
@@ -0,0 +1,41 @@
+package TelegramPackage;
+
+/**
+ *
+ * @author Daniele Roncoroni
+ */
+public class TUpdate {
+    //ATTRIBUTI
+    //ID dell'update
+    private long UpdateID;
+    //Messaggio dell'update
+    private TMessage Messaggio;
+    
+    //COSTRUTTORI
+    //Costruttore di default
+    public TUpdate(){
+        this.UpdateID = 0;
+        this.Messaggio = null;
+    }
+    //Costruttore parametrico
+    public TUpdate(long UpdateID, TMessage Messaggio){
+        this.UpdateID = UpdateID;
+        this.Messaggio = Messaggio;
+    }
+    
+    //METODI GET
+    public long getUpdateID() {
+        return UpdateID;
+    }
+    public TMessage getMessaggio() {
+        return Messaggio;
+    }
+
+    //METODI SET
+    public void setUpdateID(long UpdateID) {
+        this.UpdateID = UpdateID;
+    }
+    public void setMessaggio(TMessage Messaggio) {
+        this.Messaggio = Messaggio;
+    }
+}
